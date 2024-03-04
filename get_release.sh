5e8ac7776068b6d779310be191a9ed267450f510
sahil jain <62873731+sahilj713@users.noreply.github.com>
2024-03-04 12:16:09 +0530

    Create get_release.sh

---
+++ get_release.sh
@@ -0,0 +1,25 @@
+output=$(curl -L -s \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/releases | jq -r '.[].tag_name')
+
+github_tag=$GITHUB_REF_NAME
+
+echo $output
+match=false
+# echo $match
+for tag in $output; do
+    if [ "$tag" == "$github_tag" ]; then
+        match=true
+        break
+    fi
+done
+echo $match
+# Output the result
+if [ "$match" = true ]; then
+    echo "The selected tag has a released version."
+else
+    echo "The selected tag has not been released yet."
+    exit 1
+fi
