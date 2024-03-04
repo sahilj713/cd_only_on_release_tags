f920d23380116e55bc5dde43266d4e1b0898cebd
sahil jain <62873731+sahilj713@users.noreply.github.com>
2024-03-04 12:49:47 +0530

    Update get_release.sh

--- get_release.sh
+++ get_release.sh
@@ -1,6 +1,6 @@
 output=$(curl -L -s \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$GITHUB_REPOSITORY/releases | jq -r '.[].tag_name')
 
